6512dfe174fa1c61150731c6d11931958a05ba25
Lukas Einler Larsson <lukas.einlerlarsson@itggot.se>
2025-03-24 21:41:05 +0100

    fix: images

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 # import the Flask class from the flask module
 from flask import Flask, render_template
+from werkzeug.middleware.proxy_fix import ProxyFix
+import os
 
 # create the application object
 app = Flask(__name__)
 
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
+
 # use decorators to link the function to a url
 @app.route('/')
 def home():
-    return render_template('index.html')  # return a string
+    image_folder = os.path.join("static", "img")
+    images = [f"img/{img}" for img in os.listdir(image_folder) if img.endswith((".png", ".jpg", ".jpeg", ".gif"))]
+    return render_template('index.html', images=images)  # return a string
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -16,4 +24,5 @@ def page_not_found(e):
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    
+    app.run(debug=True, port=8000) # NOTE
